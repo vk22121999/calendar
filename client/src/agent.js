cc3e8497600401202d35b9dfd263bc7bdd6b7a2f
Venkat Kishore <avkish99@gmail.com>
2021-05-02 16:29:24 +0530

    ready-to-deploy

--- client/src/agent.js
+++ client/src/agent.js
@@ -3,7 +3,6 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'http://localhost:5000/api';
 
 
 
@@ -19,13 +18,13 @@ const tokenPlugin = req => {
 
 const requests = {
   del: url =>
-    superagent.del(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
+    superagent.del(`/api${url}`).use(tokenPlugin).then(responseBody),
   get: url =>
-    superagent.get(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
+    superagent.get(`/api${url}`).use(tokenPlugin).then(responseBody),
   put: (url, body) =>
-    superagent.put(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody),
+    superagent.put(`/api${url}`, body).use(tokenPlugin).then(responseBody),
   post: (url, body) =>
-    superagent.post(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody)
+    superagent.post(`/api${url}`, body).use(tokenPlugin).then(responseBody)
 };
 
 const Auth = {
